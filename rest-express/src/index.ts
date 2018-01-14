6395c28be1f741404cc262ea558131949cd24e97
Pradel Léo <pradel.leo@gmail.com>
2018-01-14 16:44:55 +0100

    Cleanup folder

--- rest-express/src/index.ts
+++ rest-express/src/index.ts
@@ -1,26 +1,29 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as mongoose from 'mongoose';
-import { AccountsServer } from '@accounts/server';
-import { AccountsPassword } from '@accounts/password';
-import accountsExpress from '@accounts/rest-express';
-import MongoDBInterface from '@accounts/mongo';
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
+import { AccountsServer } from "@accounts/server";
+import { AccountsPassword } from "@accounts/password";
+import accountsExpress from "@accounts/rest-express";
+import MongoDBInterface from "@accounts/mongo";
 
-mongoose.connect('mongodb://localhost:27017/js-accounts-rest-example-1');
+mongoose.connect("mongodb://localhost:27017/accounts-js-rest-example");
 const db = mongoose.connection;
 
-const app = express()
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const accountsServer = new AccountsServer({
-  db: new MongoDBInterface(db),
-  tokenSecret: 'secret',
-} as any, {
-  password: new AccountsPassword()
-});
+const accountsServer = new AccountsServer(
+  {
+    db: new MongoDBInterface(db),
+    tokenSecret: "secret"
+  },
+  {
+    password: new AccountsPassword({})
+  }
+);
 app.use(accountsExpress(accountsServer));
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });

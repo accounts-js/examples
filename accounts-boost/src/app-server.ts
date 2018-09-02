87baf1ae9d60790e7cd15eafcfbd6535002ec500
Tim Mikeladze <tim.mikeladze@gmail.com>
2018-09-02 13:45:45 -0700

    wip

--- accounts-boost/src/app-server.ts
+++ accounts-boost/src/app-server.ts
@@ -15,8 +15,12 @@ const accountsServerUri = 'http://localhost:4003/';
 (async () => {
   const accounts = (await accountsBoost({
     tokenSecret: 'terrible secret',
-    micro: true,
-  })).graphql();
+    micro: true, // setting micro to true will instruct `@accounts/boost` to only verify access tokens without any additional session logic
+  } as any)).graphql();
+
+  // Note: the following steps are optional and only required if you want to stitch the remote accounts schema with your apps schema.
+
+  // Creates a link to fetch the remote schema from the accounts microservice.
 
   const http = new HttpLink({ uri: accountsServerUri, fetch });
 
@@ -24,6 +28,8 @@ const accountsServerUri = 'http://localhost:4003/';
     if (!previousContext.graphqlContext) {
       return {};
     }
+    // Attach the accounts-access-token to requests which are proxied to the remote schema.
+    // This step is optional and only required if you want the `getUser` query to return data.
     return {
       headers: {
         'accounts-access-token': previousContext.graphqlContext.authToken,
@@ -77,7 +83,10 @@ const accountsServerUri = 'http://localhost:4003/';
   const apolloServer = await new ApolloServer({
     schema: mergeSchemas({
       schemas: [executableLocalSchema, executableRemoteSchema],
-      schemaDirectives: {},
+      schemaDirectives: {
+        // In order for the `@auth` directive to work
+        ...accounts.schemaDirectives,
+      },
     }),
     context: ({ req }) => accounts.context(req),
   }).listen();
